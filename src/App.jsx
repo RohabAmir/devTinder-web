f412bf8e8b5e9c27c408869b0b7abf5dc79182d8
Rohab_Amir <rohabamir2@gmail.com>
2025-12-26 12:24:46 +0500

    Setup the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Body from './Body'
+import Login from './Login'
+import Profile from './Profile'
 
 function App () {
   return (
     <>
-      <h1 class='text-3xl font-bold'>Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Body />}>
+            <Route
+              index
+              element={
+                <div className='m-10 text-3xl'>Welcome to DevTinder!</div>
+              }
+            />
+            <Route path='/login' element={<Login />} />
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
